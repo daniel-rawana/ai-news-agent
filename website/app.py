95b05c7e38e40ab598ca765ee4c6420f519fdd6d
Jona1227 <Jonathanserrano2734@gmail.com>
2025-10-28 05:36:14 -0400

    Setup Flask scaffold and reorganize assets
    - Created app.py scaffold with basic route
    - Moved HTML files to templates folder
    - Moved CSS and video files to static folder

---
+++ website/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
+
+
+### src for video :
+# src="{{ url_for('static', filename='test-files/test.mp4') }}"
+# href for index.html 
+#   href="{{ url_for('static', filename='AgentPageIndexStyle.css') }}"
