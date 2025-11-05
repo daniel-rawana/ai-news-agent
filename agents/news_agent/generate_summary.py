b24c46219a41182a53a7d9479473f0d5bfec2a57
bimmernico17 <justin.palma17rm@gmail.com>
2025-11-05 00:00:55 -0500

    error handling implements

--- agents/news_agent/generate_summary.py
+++ agents/news_agent/generate_summary.py
@@ -1,7 +1,7 @@
 from fetch_news import fetch_news
 from llm import summarize_story
 
-stories = fetch_news(1)
+stories = fetch_news(3)
 script = summarize_story(stories)
 
 print(script)
--- agents/news_agent/llm.py
+++ agents/news_agent/llm.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+
 def summarize_story(story):
 
     load_dotenv()
@@ -13,26 +14,52 @@ def summarize_story(story):
     if isinstance(story, str):
         story = json.loads(story)
 
-    with open("system_prompt.txt", "r", encoding="utf-8") as f:
-        system_prompt = f.read()
+    try:
+        with open("system_prompt.txt", "r", encoding="utf-8") as f:
+            system_prompt = f.read()
 
-    client = OpenAI(api_key=OPENAI_API_KEY)
+        client = OpenAI(api_key=OPENAI_API_KEY)
 
-    content = story['stories'][0]
-    user_content = "Story to summarize:\n\n"
-    user_content += (
-        f"Headline: {content['headline']}\n"
-        f"Summary: {content['summary']}\n"
-        f"Source: {content['source']}\n\n"
-    )
+        stories = story['stories'][:3]
+        user_content = "Stories to summarize:\n\n"
+        for i, content in enumerate(stories, 1):
+            user_content += (
+                f"Story {i}:\n"
+                f"Headline: {content['headline']}\n"
+                f"Summary: {content['summary']}\n"
+                f"Source: {content['source']}\n\n"
+            )
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role" :"system", "content": system_prompt},
-            {"role": "user", "content": user_content}],
-    )
+        today = datetime.now().strftime("%Y-%m-%d")
+        user_content += f"\nToday's date is: {today}"
 
-    script = response.choices[0].message.content
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_content}],
+            response_format={"type": "json_object"}
+        )
 
-    return script 
+        script = response.choices[0].message.content
+
+        return json.loads(script)
+    except Exception as e:
+        today = datetime.now().strftime("%Y-%m-%d")
+        stories = story['stories'][:3]
+        response = {
+            "date": today,
+            "stories": [
+                {
+                    "title": content['headline'],
+                    "summary": content['summary'],
+                    "source": {
+                        "name": content['source'],
+
+
+                    }
+                }
+                for content in stories
+            ]
+        }
+        return response
