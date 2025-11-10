e068ca92ee6b32615ba8b81211852cbf300ab44d
Roberto Machin <robertocmachinf@gmail.com>
2025-11-10 12:48:33 -0500

    Multiple Changes
    Started Setup For database(working on it)
    Made visual upgrades to web page, adding highlight and shadows
    Rough template for the team page added

--- website/app.py
+++ website/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+database_newsAgent = SQLAlchemy()
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "https://pjsalngjxodbkbdqoqtz.supabase.co"
 
 @app.context_processor
 def inject_hermes_assets():
@@ -35,14 +39,20 @@ def sources():
 
 @app.route("/team")
 def team():
-    return render_template('team.html')
+    linknews1 = url_for('static', filename='test-files/one.jpeg')
+    linknews2 = url_for('static', filename='test-files/two.jpg')
+    linknews3 = url_for('static', filename='test-files/three.webp')
+    linknews4 = url_for('static', filename='test-files/four.jpg')
+    linknews5 = url_for('static', filename='test-files/five.webp')
+    linknews6 = url_for('static', filename='test-files/six.jpg')
+    linknews7 = url_for('static', filename='test-files/seven.webp')
+    linknews8 = url_for('static', filename='test-files/eight.webp')
+    linknews9 = url_for('static', filename='test-files/nine.webp')
+
+    return render_template('team.html',news1 = linknews1,news2 = linknews2,news3 = linknews3,news4 = linknews4,news5 = linknews5,news6 = linknews6,news7 = linknews7,news8 = linknews8,news9 = linknews9)
 
 if __name__ == "__main__":
     # debug=True helps during development (auto-reload and better error pages)
     app.run(debug=True)
 
 
-### src for video :
-# src="{{ url_for('static', filename='test-files/test.mp4') }}"
-# href for index.html 
-#   href="{{ url_for('static', filename='AgentPageIndexStyle.css') }}"
