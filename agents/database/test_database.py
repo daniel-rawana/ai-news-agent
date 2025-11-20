972ce3bc45c77c51ca8e3384874beb5dc9a6d166
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2025-11-20 17:04:54 -0500

    Implement fetch by date function in database utils

--- agents/database/test_database.py
+++ agents/database/test_database.py
@@ -9,25 +9,25 @@ sys.path.append(str(Path(__file__).parent.parent / "news_agent"))
 from generate_summary import generate_news_script
 
 def main():
-    video_path = Path(__file__).parent.parent / "video_agent" / "Story.mp4"
+    # video_path = Path(__file__).parent.parent / "video_agent" / "Story.mp4"
 
-    video_url = database_utils.upload_video_to_storage(str(video_path))
+    # video_url = database_utils.upload_video_to_storage(str(video_path))
     
-    print(f"Public URL: {video_url}")
+    # print(f"Public URL: {video_url}")
 
-    image_path = Path(__file__).parent.parent / "video_agent" / "thumbnail.png"
+    # image_path = Path(__file__).parent.parent / "video_agent" / "thumbnail.png"
 
-    image_url = database_utils.upload_thumbnail_to_storage(str(image_path))
+    # image_url = database_utils.upload_thumbnail_to_storage(str(image_path))
 
-    print(f"Public URL: {image_url}")
+    # print(f"Public URL: {image_url}")
 
     script = generate_news_script(3)
 
-    videoID = database_utils.upload_video_metadata(script, video_url, image_url)
+    videoID = database_utils.upload_video_metadata(script)
 
     print(f"Video uploaded successfully! ID: {videoID}")
 
-    database = database_utils.fetch_all_videos()
+    database = database_utils.fetch_video_by_date("2025-11-20")
     print(json.dumps(database, indent=2))
 
 if __name__ == "__main__":
